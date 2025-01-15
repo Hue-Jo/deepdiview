0e8ccc834dd2f893269d7c6897d55a90c531530a
HueJo <slack_water@naver.com>
2025-01-15 17:11:27 +0900

    프로젝트 생성
    yml 파일은 gitignore에 넣었음

---
+++ ddv/src/main/java/community/ddv/DdvApplication.java
@@ -0,0 +1,13 @@
+package community.ddv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdvApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DdvApplication.class, args);
+  }
+
+}
