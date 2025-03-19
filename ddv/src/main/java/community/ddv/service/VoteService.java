57c7f3fc8f9a005e9008cb10c46268a0384c3be9
HueJo <slack_water@naver.com>
2025-03-19 12:11:44 +0900

    Update VoteService.java

--- ddv/src/main/java/community/ddv/service/VoteService.java
+++ ddv/src/main/java/community/ddv/service/VoteService.java
@@ -60,13 +60,13 @@ public class VoteService {
 
     // 투표 생성은 일요일만 가능, 한 주에 한 번만 가능
     LocalDateTime now = LocalDateTime.now();
-    if (now.getDayOfWeek() != DayOfWeek.TUESDAY) {
+    if (now.getDayOfWeek() != DayOfWeek.WEDNESDAY) {
       log.error("투표 생성은 일요일만 가능합니다 : 현재요일 = {}", now.getDayOfWeek());
       throw new DeepdiviewException(ErrorCode.INVALID_VOTE_CREAT_DATE);
     }
 
     // 이번주에 이미 생성된 투표가 있는지 확인
-    LocalDateTime weekStart = now.with(DayOfWeek.TUESDAY).with(LocalTime.MIN);
+    LocalDateTime weekStart = now.with(DayOfWeek.WEDNESDAY).with(LocalTime.MIN);
     LocalDateTime weekEnd = now.with(DayOfWeek.SATURDAY).with(LocalTime.MAX);
     boolean voteAlreadyExists = voteRepository.existsByStartDateBetween(weekStart, weekEnd);
     if (voteAlreadyExists) {
