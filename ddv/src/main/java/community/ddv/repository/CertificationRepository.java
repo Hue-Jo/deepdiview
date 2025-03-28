171b63e491651422e0a87fc350f9383310bb5866
HueJo <slack_water@naver.com>
2025-03-28 10:25:20 +0900

    Fix : 인증 초기화 오류 수정 _ S3에서 파일 삭제 추가
    이미 지난 인증은 더 이상 사용할 일이 없기 때문에 파일을 S3에서 삭제하는 것으로 수정했습니다.

--- ddv/src/main/java/community/ddv/repository/CertificationRepository.java
+++ ddv/src/main/java/community/ddv/repository/CertificationRepository.java
@@ -1,6 +1,5 @@
 package community.ddv.repository;
 
-import aj.org.objectweb.asm.commons.Remapper;
 import community.ddv.constant.CertificationStatus;
 import community.ddv.entity.Certification;
 import java.util.Optional;
@@ -10,7 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CertificationRepository extends JpaRepository<Certification, Long> {
@@ -18,11 +16,8 @@ public interface CertificationRepository extends JpaRepository<Certification, Lo
   boolean existsByUser_IdAndStatus(Long userId, CertificationStatus status);
   Page<Certification> findByStatus(CertificationStatus status, Pageable pageable);
 
-  // 특정 영화에 대한 인증
-  // boolean existsByUser_IdANdMovie_IdAndStatus(Long userId, Long movieId);
-
   // ENUM 타입을 NULL로 초기화 하기 위해 네이티브쿼리 사용
-  @Modifying
+  @Modifying(clearAutomatically = true, flushAutomatically = true)
   @Query(value = "UPDATE certification SET status = NULL, rejection_reason = NULL", nativeQuery = true)
   int resetAllCertifications();
 
