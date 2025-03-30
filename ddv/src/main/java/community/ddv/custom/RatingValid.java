ee9dce69cf196b62d12ec9ee180a9995230be7ee
HueJo <slack_water@naver.com>
2025-03-30 16:33:24 +0900

    Refactor : 별점은 0.5단위로 줄 수 있도록 커스텀 애너테이션 추가

---
+++ ddv/src/main/java/community/ddv/custom/RatingValid.java
@@ -0,0 +1,18 @@
+package community.ddv.custom;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = RatingValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RatingValid {
+  String message() default "별점은 0.5부터 5.0까지입니다. 0.5단위로만 줄 수 있습니다.";
+  Class<?>[] groups() default {};
+  Class<? extends Payload>[] payload() default {};
+
+}
