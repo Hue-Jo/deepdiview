6fab59afd6b1f7c7915575cf602779846b24de8f
HueJo <slack_water@naver.com>
2025-05-19 22:24:43 +0900

    refactor : swagger 설정 추가, 사용하지 않는 repository 삭제, 주석처리한 코드 삭제

--- ddv/src/main/java/community/ddv/domain/vote/repository/VoteMovieRepository.java
+++
@@ -1,8 +0,0 @@
-package community.ddv.domain.vote.repository;
-
-import community.ddv.domain.vote.entity.VoteMovie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoteMovieRepository extends JpaRepository<VoteMovie, Long> {
-
-}
