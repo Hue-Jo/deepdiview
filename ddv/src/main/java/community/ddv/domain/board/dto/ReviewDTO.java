d56cb4fcf146d78957bee8cd83825d4cda171494
HueJo <slack_water@naver.com>
2025-06-20 14:59:07 +0900

    feat : 리뷰 제목, 내용 글자 제한 유효성 검사 추가

--- ddv/src/main/java/community/ddv/domain/board/dto/ReviewDTO.java
+++ ddv/src/main/java/community/ddv/domain/board/dto/ReviewDTO.java
@@ -2,6 +2,7 @@ package community.ddv.domain.board.dto;
 
 import community.ddv.domain.board.custom.RatingValid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,9 +14,11 @@ public class ReviewDTO {
   private Long tmdbId;
 
   @NotBlank(message = "제목을 작성해주세요")
+  @Size(max = 60, message = "제목은 60자 이하로 작성해주세요")
   private String title;
 
   @NotBlank(message = "내용을 작성해주세요")
+  @Size(max = 10000, message = "내용은 10000자 이하로 작성해주세요")
   private String content;
 
   @RatingValid
@@ -27,7 +30,10 @@ public class ReviewDTO {
   @AllArgsConstructor
   public static class ReviewUpdateDTO {
 
+    @Size(max = 60, message = "제목은 60자 이하로 작성해주세요")
     private String title;
+
+    @Size(max = 10000, message = "내용은 10000자 이하로 작성해주세요")
     private String content;
 
     @RatingValid
